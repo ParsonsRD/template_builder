c8568f900e65b007d80b84df9eb31b14d801f1ab
Dan Parsons <parsonsrd@gmail.com>
2019-08-20 11:55:41 +0200

    Updated installation requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='template_builder',
-    version='0.2',
+    version='1.0',
     packages=['template_builder'],
     package_dir={'template_builder': 'template_builder'},
     package_data={'template_builder': "configs/*"},
@@ -11,5 +11,13 @@ setup(
     license='',
     author='parsonsrd',
     author_email='',
-    description='Creation tools for building ImPACT templates for ctapipe'
+    description='Creation tools for building ImPACT templates for ctapipe',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'scikit-learn',
+        'tqdm',
+        'astropy',
+        'ctapipe=0.7*',
+        'git+https://github.com/ParsonsRD/submit_SGE.git']
 )
