881bbb263de8bbd57fea8f0eef45b0a8d17e93a6
parsonsrd <parsonsrd@gmail.com>
2020-12-01 14:04:43 +0100

    Added kde to options

--- template_builder/fit_templates.py
+++ template_builder/fit_templates.py
@@ -15,7 +15,7 @@ from ctapipe.io.eventsource import event_source
 from ctapipe.reco import ImPACTReconstructor
 from scipy.interpolate import interp1d
 from tqdm import tqdm
-from ctapipe.image import tailcuts_clean
+from ctapipe.image import tailcuts_clean, dilate
 from ctapipe.calib import CameraCalibrator
 from ctapipe.image.extractor import FullWaveformSum
 
@@ -210,6 +210,11 @@ class TemplateFitter:
                     if fill_correction:
                         mask = np.logical_and(mask, mask510)
 
+                    if self.training_library == "kde" and fill_correction==False:
+                        for i in range(4):
+                            mask510 = dilate(geom, mask510)
+                        mask = np.logical_and(mask, mask510)
+
                     if amp_sum < self.min_amp:
                         continue
 
@@ -328,7 +333,11 @@ class TemplateFitter:
                 nn_out = model(grid.T)
                 nn_out = nn_out.reshape((self.bins[1], self.bins[0]))
                 nn_out[np.isinf(nn_out)] = 0
-
+            elif self.training_library == "kde":
+                points, nn_out = model.evaluate((self.bins[0], self.bins[1]))#grid.T)
+                # rint(nn_out)
+                nn_out = nn_out.reshape((self.bins[0], self.bins[1]))
+                nn_out[np.isinf(nn_out)] = 0
             else:
                 # Evaluate MLP fit over our grid
                 nn_out = model.predict(grid.T)
@@ -399,6 +408,15 @@ class TemplateFitter:
                                  n_iter_no_change=10)
 
             model.fit(pixel_pos, amp)
+        elif training_library == "kde":
+            from KDEpy import FFTKDE
+            from scipy.interpolate import LinearNDInterpolator
+
+            kde = FFTKDE(bw=0.07).fit(pixel_pos, weights=amp)
+            points, out = kde.evaluate((self.bins[0], self.bins[1]))
+            #print(points.shape, points)
+            lin = LinearNDInterpolator(points, out, fill_value=0)
+            return lin
 
         elif training_library == "KNN":
             from sklearn.neighbors import KNeighborsRegressor
@@ -628,8 +646,11 @@ class TemplateFitter:
                 correction_factor = np.median(self.correction[key])
                 correction_factor_error = np.std(self.correction[key]) / np.sqrt(float(len(self.correction[key])))
 
-                if correction_factor > 0 and correction_factor_error / correction_factor < 0.2:
+                print(correction_factor_error / correction_factor, correction_factor)
+                if correction_factor > 0 and correction_factor_error / correction_factor < 0.1:
                     self.template_fit[key] = self.template_fit[key] * np.median(self.correction[key])
+                else:
+                    self.template_fit.pop(key)
 
         file_handler = gzip.open(output_file, "wb")
         pickle.dump(self.template_fit, file_handler)
