6919b7bfa0488f478e8340ad05b58d50db8b3f5d
gschwefer <georg.schwefer@mpi-hd.mpg.de>
2023-05-05 10:14:04 +0200

    Added tool to merge templates

---
+++ template_builder/merge_templates.py
@@ -0,0 +1,100 @@
+from ctapipe.core import Tool, traits
+
+from ctapipe.core.traits import List, Unicode
+
+
+from argparse import ArgumentParser
+from pathlib import Path
+
+import sys
+import gzip
+import pickle
+
+
+class TemplateMerger(Tool):
+
+    """
+    Tool to merge multiple templates dictionaries each with a different
+    subset of keys into one large template dictionary.
+    This is useful mostly because creating multiple subsets in parallel is much
+    faster computationally than directly creating one large dictionary.
+    """
+
+    input_dir = traits.Path(
+        default_value=None,
+        help="Input directory",
+        allow_none=True,
+        exists=True,
+        directory_ok=True,
+        file_ok=False,
+    ).tag(config=True)
+
+    input_files = List(
+        traits.Path(exists=True, directory_ok=False),
+        default_value=[],
+        help="Input template dictionary files",
+    ).tag(config=True)
+
+    file_pattern = Unicode(
+        default_value="*.template.gz",
+        help="Give a specific file pattern for matching files in ``input_dir``",
+    ).tag(config=True)
+
+    parser = ArgumentParser()
+    parser.add_argument("input_files", nargs="*", type=Path)
+
+    output_file = Unicode(default_value=".", help="base output file name").tag(
+        config=True
+    )
+
+    def setup(self):
+
+        # Load in all input files
+        args = self.parser.parse_args(self.extra_args)
+        self.input_files.extend(args.input_files)
+        if self.input_dir is not None:
+            self.input_files.extend(sorted(self.input_dir.glob(self.file_pattern)))
+
+        if not self.input_files:
+            self.log.critical(
+                "No input files provided, either provide --input-dir "
+                "or input files as positional arguments"
+            )
+            sys.exit(1)
+
+    def start(self):
+        self.full_template_dict = {}
+        # Open and unpickle the tempalte ditionaries
+        for input_file in self.input_files:
+            this_dict = None
+            with gzip.open(input_file, "r") as gz_inp_file:
+                this_dict = pickle.load(gz_inp_file)
+
+            for key, value in this_dict.items():
+                # We need to make sure that we have a unique template for every key.
+                # It is at this stage not obvious how to combine two templates for the same key into one.
+                # Therefore, we throw an error as soon as a key that already exists is accessed again.
+                assert (
+                    key not in self.full_template_dict.keys()
+                ), "The key {} is already in the merged template dictionary.".format(
+                    key
+                )
+                # Write the dictionary to the common dictionary.
+                self.full_template_dict[key] = value
+
+    def finish(self):
+        # Save the combined template
+        file_handler = gzip.open(self.output_file + ".template.gz", "wb")
+        pickle.dump(self.full_template_dict, file_handler)
+        file_handler.close()
+
+
+def main():
+    """run the tool"""
+
+    tool = TemplateMerger()
+    tool.run()
+
+
+if __name__ == "__main__":
+    main()
